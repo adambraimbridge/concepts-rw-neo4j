edf8ed486bc00d90d3732c07d37dd866aa36bbbc
Emil Dimitrov <emil.dimitrov@ft.com>
2019-03-21 11:07:17 +0200

    Add ISO13661 to types

--- concepts/concepts_service.go
+++ concepts/concepts_service.go
@@ -114,6 +114,8 @@ type neoAggregatedConcept struct {
 	YearFounded            int      `json:"yearFounded,omitempty"`
 	LeiCode                string   `json:"leiCode,omitempty"`
 	ParentOrganisation     string   `json:"parentOrganisation,omitempty"`
+	// Location
+	ISO13661 string `json:"iso13661,omitempty"`
 	// Person
 	Salutation string `json:"salutation,omitempty"`
 	BirthYear  int    `json:"birthYear,omitempty"`
@@ -161,6 +163,8 @@ type neoConcept struct {
 	YearFounded            int      `json:"yearFounded,omitempty"`
 	LeiCode                string   `json:"leiCode,omitempty"`
 	ParentOrganisation     string   `json:"parentOrganisation,omitempty"`
+	// Location
+	ISO13661 string `json:"iso13661,omitempty"`
 	// Person
 	Salutation string `json:"salutation,omitempty"`
 	BirthYear  int    `json:"birthYear,omitempty"`
